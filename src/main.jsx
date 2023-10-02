48592cff6e663a2bf12381b5edaaa5363e018de4
Alex Daniel <1151372@senati.pe>
2023-10-01 22:10:27 -0500

    Cambiando el logo de mi website

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,18 @@
 import "./utils/darkMode";
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import { RouterProvider } from 'react-router-dom'
-import { router } from './router/Routes'
-import { UserProvider } from './context/userContext'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./router/Routes";
+import { UserProvider } from "./context/userContext";
 import { SnackbarProvider } from "notistack";
+import logo from './assets/img/logo/logo.png'
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <UserProvider>
-        <SnackbarProvider autoHideDuration={2000} maxSnack={3} >
-    <RouterProvider router={router} />
-        </SnackbarProvider>
-    </UserProvider>
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <UserProvider>
+    <link rel="shortcut icon" href={logo} type="image/x-icon" />
+    <SnackbarProvider autoHideDuration={2000} maxSnack={3}>
+      <RouterProvider router={router} />
+    </SnackbarProvider>
+  </UserProvider>
+);
